909aed12e34f8cbdfb83c08a45c81b28807b7f25
Alejandro Pobes <73605642+Xabian-hub@users.noreply.github.com>
2024-11-06 16:11:09 +0100

    Creacion Java Class 1-18

--- src/main/java/com/alejandropobes/tema05/Main.java
+++ src/main/java/com/alejandropobes/tema05/Main.java
@@ -2,6 +2,6 @@ package com.alejandropobes.tema05;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
     }
 }
---
+++ src/main/java/com/alejandropobes/tema05/strings/Ejercicio14.java
@@ -0,0 +1,4 @@
+package com.alejandropobes.tema05.strings;
+
+public class Ejercicio14 {
+}
