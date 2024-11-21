e1b0aa3ee624df8c06a64b0f687fc3c12fbb3b1a
Alejandro Pobes <73605642+Xabian-hub@users.noreply.github.com>
2024-11-21 19:01:50 +0100

    Ejericio 11

--- src/main/java/com/alejandropobes/tema05/arrays/Ejercicio11.java
+++ src/main/java/com/alejandropobes/tema05/arrays/Ejercicio11.java
@@ -1,4 +1,31 @@
 package com.alejandropobes.tema05.arrays;
 
+import java.util.Arrays;
+
 public class Ejercicio11 {
+
+    /**
+     * Invierte el contenido de un array.
+     * @param original Array original.
+     * @return Nuevo array con los elementos en orden inverso.
+     */
+    public static int[] invertirArray(int[] original) {
+        int[] invertido = new int[original.length];
+        for (int i = 0; i < original.length; i++) {
+            invertido[i] = original[original.length - 1 - i];
+        }
+        return invertido;
+    }
+
+    public static void main(String[] args) {
+        final int LONGITUD = 10; // Tamaño del array
+        int[] V = {-1, 30, 20, 1, 5, 100, -5, 7, -100, 1000};
+        System.out.println("Array original (V): " + Arrays.toString(V));
+
+        // Generar array P con los elementos de V en orden inverso
+        int[] P = invertirArray(V);
+
+        // Visualizar ambos arrays
+        System.out.println("Array invertido (P): " + Arrays.toString(P));
+    }
 }
